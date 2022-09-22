09ad145e09e19a6565e692f80a6ee2e70b4f34b8
aingbager <aingbager55@gmail.com>
2022-09-22 21:57:06 +0700

    add prototype

--- prototype/prototype1.cpp
+++ prototype/prototype1.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-class Orang{
+class Player{
 public:
-	string name;
+	string nama;
 
-	Orang(const char* nama);
-	void Tampilkan(){
-	co
+//prototype
+
+	Player(const char*);
+	void Readd();
+	void ubahName(const char*);
+};
+
+int main(){
+	Player* ucup = new Player("ucup");
+	ucup->Readd();
+	ucup->ubahName("udin");
+	ucup->Readd();
+
+	return 0;
+}
+
+//penjabarannya
+
+Player::Player(const char* nama){
+	this->nama = nama;
+}
+
+void Player::Readd(){
+	cout << this->nama << endl;
+}
+void Player::ubahName(const char* ubahNama){
+	this->nama = ubahNama;
+}
